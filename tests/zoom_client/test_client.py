e049d96e64011f55d93cbfc86e62efc4f27daee5
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2022-09-05 09:50:11 +0200

    Add init_from_env and tests

--- tests/zoom_client/test_client.py
+++ tests/zoom_client/test_client.py
@@ -1,2 +1,19 @@
-def test_first():
-    assert 5 == 5
+from zoom_client.client import ZoomClient
+
+
+def test_client():
+    client = ZoomClient("aaa", "bbb", "ccc")
+    assert client.account_id == "aaa"
+    assert client.client_id == "bbb"
+    assert client.client_secret == "ccc"
+
+
+def test_init_from_env(monkeypatch):
+    monkeypatch.setenv("ZOOM_ACCOUNT_ID", "aaa")
+    monkeypatch.setenv("ZOOM_CLIENT_ID", "bbb")
+    monkeypatch.setenv("ZOOM_SECRET_ID", "ccc")
+
+    client = ZoomClient.init_from_env()
+    assert client.account_id == "aaa"
+    assert client.client_id == "bbb"
+    assert client.client_secret == "ccc"
--- zoom_client/client.py
+++ zoom_client/client.py
@@ -1,4 +1,24 @@
+import os
+
+
+class ZoomClientError(Exception):
+    pass
+
+
 class ZoomClient:
-    def __init__(self, client_id: str, client_secret: str):
+    def __init__(self, account_id: str, client_id: str, client_secret: str):
+        self.account_id = account_id
         self.client_id = client_id
         self.client_secret = client_secret
+
+    @staticmethod
+    def init_from_env():
+        account_id = os.getenv("ZOOM_ACCOUNT_ID", default=None)
+        client_id = os.getenv("ZOOM_CLIENT_ID", default=None)
+        client_secret = os.getenv("ZOOM_SECRET_ID", default=None)
+        if account_id and client_id and client_secret:
+            zoom_client = ZoomClient(account_id, client_id, client_secret)
+            return zoom_client
+        raise ZoomClientError(
+            "ZOOM_ACCOUNT_ID, ZOOM_CLIENT_ID or ZOOM_SECRET_ID are not properly set in the environment variables"
+        )

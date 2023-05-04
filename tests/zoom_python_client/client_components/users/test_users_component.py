0953353f438677b8468ebc4f8f4b08c91fdd15fe
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2023-05-04 21:22:37 +0200

    Add simple test for get_user_meetings

--- tests/zoom_python_client/client_components/users/test_users_component.py
+++ tests/zoom_python_client/client_components/users/test_users_component.py
@@ -1,4 +1,5 @@
 import responses
+
 from tests.zoom_python_client.base_test_case import TestCaseWithAuth
 from zoom_python_client.client_components.users.users_component import UsersComponent
 from zoom_python_client.zoom_api_client import ZoomApiClient
@@ -6,7 +7,7 @@ from zoom_python_client.zoom_api_client import ZoomApiClient
 
 class TestUsersComponent(TestCaseWithAuth):
     @responses.activate
-    def test_users_component(self):
+    def test_get_user(self):
         responses.add(
             responses.GET,
             "http://localhost/users/12345",
@@ -16,4 +17,17 @@ class TestUsersComponent(TestCaseWithAuth):
         zoom_client = ZoomApiClient("aaa", "bbb", "ccc", "http://localhost")
         users_component = UsersComponent(zoom_client)
         user = users_component.get_user("12345")
+        assert user == {"response": "ok"}
+
+    @responses.activate
+    def test_get_user_meetings(self):
+        responses.add(
+            responses.GET,
+            "http://localhost/users/12345/meetings/",
+            json={"response": "ok"},
+            status=200,
+        )
+        zoom_client = ZoomApiClient("aaa", "bbb", "ccc", "http://localhost")
+        users_component = UsersComponent(zoom_client)
+        user = users_component.get_user_meetings("12345", {})
         assert user == {"response": "ok"}

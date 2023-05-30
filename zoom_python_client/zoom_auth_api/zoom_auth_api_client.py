06fc88aed36480cae2003e3965b0348ab9d1d69f
René Fernández <rene.fernandez@cern.ch>
2023-05-30 09:47:10 +0200

    Load and save access token from/to file (#16)
    * Feature/retry on 401 (#8)
    
    * Version 0.0.4
    
    * Improve logging
    
    * Set logging handler to NullHandler
    
    * Update logging params to constructor
    
    * Verify response is not None
    
    * Update examples
    
    * Log when token is not on environment
    
    * Update urls and document how to configure logging
    
    * Add test for setup_logs
    
    * Update app classifier for pypi
    
    * Lower requests req version to 2.23.0 (#11)
    
    * Feature/use poetry (#13)
    
    * Use poetry and add publish action
    
    * Add mantainers, urls and update license name
    
    * Update publish actions
    
    * Feature/use poetry (#14)
    
    * Use poetry and add publish action
    
    * Add mantainers, urls and update license name
    
    * Update publish actions
    
    * Bump version to 0.0.6-beta.1
    
    * Use beta on Pypi
    
    * Feature/load from file (#15)
    
    * Add poetry.toml
    
    * Load and save access token on file
    
    * Bump version to 0.0.6
    
    * Add example for set/get token on/from file
    
    * Fix expire_time tests
    
    * Fix coverage file
    
    * Add tests for new file options
    
    * Rename from_path to use_path
    
    * Add use_path docs

--- zoom_python_client/zoom_auth_api/zoom_auth_api_client.py
+++ zoom_python_client/zoom_auth_api/zoom_auth_api_client.py
@@ -2,6 +2,7 @@ import logging
 import os
 from base64 import b64encode
 from time import time
+from typing import Optional
 
 from zoom_python_client.api_client import ApiClient
 
@@ -16,11 +17,18 @@ class ZoomAuthApiClient:
     oauth_base_url: str = "https://zoom.us/oauth"
     minimum_expire_time_seconds: int = 300
 
-    def __init__(self, account_id: str, client_id: str, client_secret: str):
+    def __init__(
+        self,
+        account_id: str,
+        client_id: str,
+        client_secret: str,
+        use_path: Optional[str] = None,
+    ):
         self.account_id = account_id
         self.client_id = client_id
         self.client_secret = client_secret
         self.authentication_client = ApiClient(self.oauth_base_url)
+        self.use_path = use_path
 
     def base64_encode_auth(self):
         encoded_auth = b64encode(
@@ -28,8 +36,7 @@ class ZoomAuthApiClient:
         ).decode()
         return encoded_auth
 
-    def is_zoom_access_token_expired(self):
-        expire_seconds = os.getenv("ZOOM_ACCESS_TOKEN_EXPIRE", default=None)
+    def is_zoom_access_token_expired(self, expire_seconds):
         current_seconds = int(time())
 
         if expire_seconds:
@@ -50,17 +57,37 @@ class ZoomAuthApiClient:
         logger.debug("Auth headers generated")
         return headers
 
+    def save_token_and_seconds_to_env(self, result):
+        try:
+            os.environ["ZOOM_ACCESS_TOKEN"] = result["access_token"]
+            os.environ["ZOOM_ACCESS_TOKEN_EXPIRE"] = str(
+                int(time()) + int(result["expires_in"])
+            )
+        except ValueError as error:
+            raise ZoomAuthApiClientError(
+                "Unable to set access_token expiration. expires_in is not an int"
+            ) from error
+
+    def save_token_and_seconds_to_file(self, result):
+        # Save the token to a file
+        if self.use_path is None:
+            raise ZoomAuthApiClientError("use_path is None")
+        file_path = os.path.join(self.use_path, "access_token")
+        with open(file_path, "w") as token_file:
+            token_file.write(result["access_token"])
+        # Save the expire_seconds to a file
+        file_path = os.path.join(self.use_path, "expire_seconds")
+        with open(file_path, "w") as token_file:
+            new_expire = str(int(time()) + int(result["expires_in"]))
+            token_file.write(new_expire)
+        return True
+
     def extract_access_token(self, result):
         if "access_token" in result and "expires_in" in result:
-            try:
-                os.environ["ZOOM_ACCESS_TOKEN"] = result["access_token"]
-                os.environ["ZOOM_ACCESS_TOKEN_EXPIRE"] = str(
-                    int(time()) + int(result["expires_in"])
-                )
-            except ValueError as error:
-                raise ZoomAuthApiClientError(
-                    "Unable to set access_token expiration. expires_in is not an int"
-                ) from error
+            if self.use_path:
+                self.save_token_and_seconds_to_file(result)
+            else:
+                self.save_token_and_seconds_to_env(result)
             logger.debug("Access token extracted from oauth response")
             return result["access_token"]
 
@@ -76,3 +103,27 @@ class ZoomAuthApiClient:
         result = response.json()
         access_token = self.extract_access_token(result)
         return access_token
+
+    def get_access_token_from_file(self):
+        if self.use_path is None:
+            raise ZoomAuthApiClientError("use_path is None")
+        # join the path to the file name
+        file_path = os.path.join(self.use_path, "access_token")
+        try:
+            with open(file_path, "r") as token_file:
+                access_token = token_file.read()
+                return access_token
+        except FileNotFoundError:
+            return None
+
+    def get_expire_seconds_from_file(self):
+        # join the path to the file name
+        if self.use_path is None:
+            raise ZoomAuthApiClientError("use_path is None")
+        file_path = os.path.join(self.use_path, "expire_seconds")
+        try:
+            with open(file_path, "r") as token_file:
+                access_token = token_file.read()
+                return access_token
+        except FileNotFoundError:
+            return None

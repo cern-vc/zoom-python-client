5349b0208b2b0d7122d20dc670f6b4e3af9b55d9
Tomas Roun <tomas.roun@cern.ch>
2023-06-01 13:59:19 +0200

    Simplify get_project_dir()

--- zoom_python_client/utils/file_system.py
+++ zoom_python_client/utils/file_system.py
@@ -1,9 +1,5 @@
-import os
 from pathlib import Path
 
 
 def get_project_dir():
-    current_dir = os.path.abspath(os.path.dirname(__file__))
-    path = Path(current_dir)
-    project_dir = path.parent.absolute().parent
-    return project_dir
+    return Path(__file__).resolve().parents[2]
